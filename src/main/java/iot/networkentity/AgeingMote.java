62f94f5a707e387b6ecda844af9045125a86b242
Dmitrijs Voronovs <dmitrijs.voronovs@sapiens.com>
2022-11-17 16:30:47 +0100

    Add ageing mote class

---
+++ src/main/java/iot/networkentity/AgeingMote.java
@@ -0,0 +1,15 @@
+package iot.networkentity;
+
+import iot.Environment;
+import util.Path;
+
+import java.util.List;
+
+public class AgeingMote extends Mote {
+    private Integer age;
+
+    public AgeingMote(long DevEUI, double xPos, double yPos, int transmissionPower, int SF, List<MoteSensor> moteSensors, int energyLevel, Path path, double movementSpeed, int startMovementOffset, int periodSendingPacket, int startSendingOffset, Environment environment, Integer age) {
+        super(DevEUI, xPos, yPos, transmissionPower, SF, moteSensors, energyLevel, path, movementSpeed, startMovementOffset, periodSendingPacket, startSendingOffset, environment);
+        this.age = age;
+    }
+}
