a9223cb1f7db817d095b7ee5964bac9065a2c8b1
Dmitrijs Voronovs <dmitrijs.voronovs@sapiens.com>
2022-12-17 04:09:35 +0100

    Update AgingMote field types

--- src/IotDomain/AgingMote.java
+++ src/IotDomain/AgingMote.java
@@ -3,13 +3,13 @@ package IotDomain;
 import org.jxmapviewer.viewer.GeoPosition;
 
 import java.util.LinkedList;
+import java.util.Random;
 
 public class AgingMote extends Mote {
     /**
-     * TODO: decide in minutes / seconds / mseconds / months ??
-     * Should be based upon the reference value we choose for time tracking
+     * Data type corresponds with the time delta for each tik of the Simulation
      */
-    protected float age = 0;
+    protected long age = 0;
 
     /**
      * Increases with the age
@@ -27,7 +27,7 @@ public class AgingMote extends Mote {
         return totalEnergyConsumed;
     }
 
-    public float getAgeingFactor() {
+    public float getAgingFactor() {
         return ageingFactor;
     }
 
@@ -35,17 +35,26 @@ public class AgingMote extends Mote {
         this.ageingFactor = ageingFactor;
     }
 
-    protected float getAge() {
+    public long getAge() {
         return age;
     }
 
-    protected void setAge(float age) {
+    protected void setAge(long age) {
         this.age = age;
     }
 
-    public AgingMote(Long DevEUI, Integer xPos, Integer yPos, Environment environment, Integer transmissionPower, Integer SF, LinkedList<MoteSensor> moteSensors, Integer energyLevel, LinkedList<GeoPosition> path, Integer samplingRate, Double movementSpeed, Integer startOffset, float age) {
+    public void increaseAgeBy(long ageDelta) { this.age += ageDelta; }
+
+    public AgingMote(Long DevEUI, Integer xPos, Integer yPos, Environment environment, Integer transmissionPower, Integer SF, LinkedList<MoteSensor> moteSensors, Integer energyLevel, LinkedList<GeoPosition> path, Integer samplingRate, Double movementSpeed, Integer startOffset, long initialAge, float initalTotalEnergyConsumed) {
         super(DevEUI, xPos, yPos, environment, transmissionPower, SF, moteSensors, energyLevel, path, samplingRate, movementSpeed, startOffset);
-        setAge(age);
+        environment.addMote(this);
+        setAge(initialAge);
+        setTotalEnergyConsumed(initalTotalEnergyConsumed);
+    }
+
+    public AgingMote(Long DevEUI, Integer xPos, Integer yPos, Environment environment, Integer transmissionPower,
+                Integer SF, LinkedList<MoteSensor> moteSensors, Integer energyLevel, LinkedList<GeoPosition> path, Integer samplingRate, Double movementSpeed){
+        this(DevEUI,xPos,yPos, environment,transmissionPower,SF,moteSensors,energyLevel,path,samplingRate, movementSpeed, Math.abs((new Random()).nextInt(5)),0,0);
     }
 
     @Override
