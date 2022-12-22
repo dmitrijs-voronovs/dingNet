2713bb2d83159c7ba5e640176800a83bb6a48114
Dmitrijs Voronovs <dmitrijs.voronovs@sapiens.com>
2022-12-22 18:15:47 +0100

    Update constants to use Durations instead of Periods

--- src/IotDomain/Constants.java
+++ src/IotDomain/Constants.java
@@ -1,16 +1,14 @@
 package IotDomain;
 
 import java.time.Duration;
-import java.time.Period;
 import java.util.Calendar;
 
 public class Constants {
-    public static final Period DEVICE_ADJUSTMENT_RATE = Period.ofDays(1);
+    public static final Duration DEVICE_ADJUSTMENT_RATE = Duration.ofDays(1);
     public static final Calendar SIMULATION_BEGINNING;
     public static final float COMPENSATION_COEFFICIENT = 0.9f;
-    public static final Period DEVICE_LIFESPAN = Period.ofYears(10);
-//    public static final Period DEVICE_LIFESPAN2 = Time.from();
-    public static final Duration SIMULATION_TIME_MEASUREMENT = Duration.ofDays(1);
+    public static final Duration DEVICE_LIFESPAN = Duration.ofDays(365 * 15);
+    public static final Duration SIMULATION_TIME_MEASUREMENT = Duration.ofHours(1);
 
 
     static {
