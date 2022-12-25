adcd84458a138e506fa434696c28634695288fd6
Dmitrijs Voronovs <dmitrijs.voronovs@sapiens.com>
2022-12-25 19:02:59 +0100

    Update constants

--- src/IotDomain/Constants.java
+++ src/IotDomain/Constants.java
@@ -4,7 +4,10 @@ import java.time.Duration;
 import java.util.Calendar;
 
 public class Constants {
-    public static final Duration DEVICE_ADJUSTMENT_RATE = Duration.ofHours(2);
+    /**
+     * Should always be a single month!
+     */
+    public static final Duration DEVICE_ADJUSTMENT_RATE = Duration.ofDays(30);
     public static final Calendar SIMULATION_BEGINNING;
     public static final float COMPENSATION_COEFFICIENT = 0.9f;
     public static final Duration DEVICE_LIFESPAN = Duration.ofDays(365 * 15);
